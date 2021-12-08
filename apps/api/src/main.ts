c8a89e14cf7548f2b4921092fb2ab547ff219512
Albert Choi <acchoi@gmail.com>
2021-12-08 13:11:12 -0500

    Set up proxy to backend and API

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,18 +1,31 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
+import * as dotenv from 'dotenv';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+
+dotenv.config();
+
+const HARVEST_TOKEN = process.env.HARVEST_TOKEN;
+const HARVEST_ACCOUNT_ID = process.env.HARVEST_ACCOUNT_ID;
+const USER_AGENT = 'HarvestGraphs (acchoi@gmail.com)';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+const apiProxy = createProxyMiddleware({
+  target: 'https://api.harvestapp.com',
+  changeOrigin: true,
+  secure: false,
+  onProxyReq: (proxyReq, req, res) => {
+    proxyReq.setHeader('user-agent', USER_AGENT);
+    proxyReq.setHeader('authorization', `Bearer ${HARVEST_TOKEN}`);
+    proxyReq.setHeader('harvest-account-id', HARVEST_ACCOUNT_ID);
+
+  },
+  logLevel: 'debug'
 });
+app.use('/v2', apiProxy);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
